bdf099bbedaab49761313d8f604e74f2e543ae2b
Sean B <butts.sean01@gmail.com>
2020-06-03 07:23:05 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here 
-def say_hello(name="sean")
-  print "hello #{"name"}"
+def say_hello(name="Ruby Programmer")
+  print "hello #{name}"
 end
 
-say_hello
+say_hello("Sean")
